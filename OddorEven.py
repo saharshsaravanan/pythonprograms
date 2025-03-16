efecf60f1e50e604951902a14a9b6cd5e308cdf3
Saharsh <mailidofsaharshsaravanan@gmail.com>
2025-03-16 11:57:34 -0400

    The program is to find out is a number odd or even this works till 1 with 4300 zeroes

--- OddorEven.py
+++ OddorEven.py
@@ -1,13 +1,9 @@
-# write a program to print the given number is odd or evem
-while True:
-    try:
-        num = int(input("Enter a number: "))  # Prompt user for a number and convert input to integer
-        if num % 2 == 0:
-            print(f"You entered {num}, which is even.")  # Corrected 'Print' to 'print' and fixed concatenation
-            break
-        else:
-            print(f"You entered {num}, which is odd.")  # Corrected 'Print' to 'print'
-            break
-    except ValueError:
-        print("Please enter a valid number.")  # Handle non-integer inputs
+# write a program to print the given number is odd or even
+# Assume user will enter only number
 
+num = int(input("Enter a number "))
+result = "The given number is "
+if num %2 == 1:
+    print(result +"  odd. "  )
+else :
+    print(result + "even.")
