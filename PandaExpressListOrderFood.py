1f70dd51736615ec92f3a122862dbb16844b200c
Saharsh <mailidofsaharshsaravanan@gmail.com>
2025-03-21 21:32:34 -0400

    panda express

---
+++ PandaExpressListOrderFood.py
@@ -0,0 +1,54 @@
+import total
+
+
+def printMenu(menu):
+    for key in menu:
+        print(key)
+
+
+total=0
+pandalist=[]
+menudictonary={"bowl":8
+               ,"plate":10
+               ,"bigger plate":13,
+               "family meal":35}
+print("Welcome to panda express")
+print("---------------------------")
+
+
+printMenu(menudictonary)
+
+while True:
+    food=input("What do you want from the menu q to quit r to reset \n")
+    print (food.upper())
+    if (food.upper() == "Q"):
+        break
+        print(" please enter a item from the menu ")
+    elif (food.lower() == "r"):
+        pandalist.clear()
+    elif food not in menudictonary.keys():
+        printMenu(menudictonary)
+    else:
+        pandalist.append(food)
+
+
+for food in pandalist :
+    print(food)
+    print(f"${(menudictonary.get(food)):.2f}",end=" " )
+    total+=(menudictonary.get(food))
+
+print(f"your total is ${total:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
